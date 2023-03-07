9ecb5240e18acdbe21c2533e2d49f71f443f8991
FrankieLv <32904224+FrankieLv@users.noreply.github.com>
2023-03-07 18:56:38 +0800

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,31 +6,5 @@ import { MatchValidator } from './_helpers/must-match.validator';
 
 @Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent implements OnInit {
-  form: FormGroup;
-  submitted = false;
-  loading = false;
-
-  constructor(private formBuilder: FormBuilder) {}
-
-  ngOnInit() {
-    this.form = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      username: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
-  }
-
-  get f() {
-    return this.form.controls;
-  }
-
-  onSubmit() {
-    this.submitted = true;
-    this.submitted = true;
-    this.loading = true;
-    if (this.form.invalid) {
-      return;
-    }
-  }
+  ngOnInit() {}
 }
